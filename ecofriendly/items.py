6abda1804ddf4726c1c911ba9b4ba5553b3e67c4
rifatrakib <Rakib.1508@outlook.com>
2022-05-24 00:23:37 +0600

    reshaped fusion output using items and pipelines

--- ecofriendly/items.py
+++ ecofriendly/items.py
@@ -2,7 +2,7 @@ import scrapy
 from itemloaders.processors import TakeFirst, MapCompose
 
 
-def stringify(items):
+def stringify_list(items):
     processed_items = []
     for item in items:
         processed_items.append(item.strip())
@@ -13,10 +13,13 @@ def stringify(items):
 class EcofriendlyItem(scrapy.Item):
     name = scrapy.Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst)
     description = scrapy.Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst)
-    details = scrapy.Field(input_processor=MapCompose(stringify), output_processor=TakeFirst)
-    categories = scrapy.Field(input_processor=MapCompose(stringify), output_processor=TakeFirst)
-    tags = scrapy.Field(input_processor=MapCompose(stringify), output_processor=TakeFirst)
+    details = scrapy.Field(input_processor=MapCompose(stringify_list), output_processor=TakeFirst)
+    categories = scrapy.Field(input_processor=MapCompose(stringify_list), output_processor=TakeFirst)
+    tags = scrapy.Field(input_processor=MapCompose(stringify_list), output_processor=TakeFirst)
 
 
 class FusionItem(scrapy.Item):
-    pass
+    product_name = scrapy.Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst)
+    details = scrapy.Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst)
+    uses = scrapy.Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst)
+    specifications = scrapy.Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst)
--- ecofriendly/spiders/fusion_spider.py
+++ ecofriendly/spiders/fusion_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from ecofriendly.items import FusionItem
 
 
 class FusionSpider(scrapy.Spider):
@@ -6,20 +7,7 @@ class FusionSpider(scrapy.Spider):
     allowed_domains = ['fusionpkg.com']
     start_urls = ['https://fusionpkg.com/shop/']
     
-    def parse(self, response):
-        for link in response.css('a.learn-more-button::attr(href)'):
-            yield response.follow(link.get(), callback=self.parse_item)
-        
-        next_page = response.css('a.next::attr(href)').get()
-        if next_page:
-            yield response.follow(next_page, callback=self.parse)
-    
-    def parse_item(self, response):
-        product_name = response.css('h1.product_title::text').get()
-        details = response.css('div.et_pb_module_inner p::text').get()
-        uses = response.css('div.et_pb_text_3 div.et_pb_text_inner::text').get()
-        
-        specs = response.css('div.specificaion-inner-right ul li')
+    def extract_specifications_data(self, specs):
         data = {}
         for spec in specs:
             items = spec.css('li')
@@ -36,9 +24,41 @@ class FusionSpider(scrapy.Spider):
                     else:
                         data[key] = item.css('span:nth-child(2)::text').get()
         
-        yield {
-            'product_name': product_name,
-            'details': details,
-            'uses': uses,
-            'specifications': data,
-        }
+        return data
+    
+    def stringify_dict(self, data):
+        processed_items = []
+        count = 1
+        for key, value in data.items():
+            if key.endswith('#'):
+                key = key.replace('#', 'number')
+            key = key.lower().replace(' ', '_')
+            item_data = f'{count}. {key}: {value}'
+            processed_items.append(item_data)
+            count += 1
+        
+        return '\n'.join(processed_items)
+    
+    def parse(self, response):
+        for link in response.css('a.learn-more-button::attr(href)'):
+            yield response.follow(link.get(), callback=self.parse_item)
+        
+        next_page = response.css('a.next::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+    
+    def parse_item(self, response):
+        product_name = response.css('h1.product_title::text').get()
+        details = response.css('div.et_pb_module_inner p::text').get()
+        uses = response.css('div.et_pb_text_3 div.et_pb_text_inner::text').get()
+        
+        specs = response.css('div.specificaion-inner-right ul li')
+        data = self.extract_specifications_data(specs)
+        processed_data = self.stringify_dict(data)
+        
+        item = FusionItem()
+        item['product_name'] = product_name
+        item['details'] = details
+        item['uses'] = uses
+        item['specifications'] = processed_data
+        yield item
