40fc19c4f9556e0fee5c77e86c1f55f96820844f
rifatrakib <Rakib.1508@outlook.com>
2022-05-23 17:44:27 +0600

    implemented crawl logic for fusion spider

--- ecofriendly/spiders/fusion_spider.py
+++ ecofriendly/spiders/fusion_spider.py
@@ -7,4 +7,12 @@ class FusionSpider(scrapy.Spider):
     start_urls = ['https://fusionpkg.com/shop/']
     
     def parse(self, response):
+        for link in response.css('a.learn-more-button::attr(href)'):
+            yield response.follow(link.get(), callback=self.parse_item)
+        
+        next_page = response.css('a.next::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+    
+    def parse_item(self, response):
         pass
