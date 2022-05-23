92fc56001b5d3a11aa5d1a80bd639c76fd6765ad
rifatrakib <Rakib.1508@outlook.com>
2022-05-23 22:10:28 +0600

    fusion spider data extraction logic implemented

--- ecofriendly/spiders/fusion_spider.py
+++ ecofriendly/spiders/fusion_spider.py
@@ -15,4 +15,30 @@ class FusionSpider(scrapy.Spider):
             yield response.follow(next_page, callback=self.parse)
     
     def parse_item(self, response):
-        pass
+        product_name = response.css('h1.product_title::text').get()
+        details = response.css('div.et_pb_module_inner p::text').get()
+        uses = response.css('div.et_pb_text_3 div.et_pb_text_inner::text').get()
+        
+        specs = response.css('div.specificaion-inner-right ul li')
+        data = {}
+        for spec in specs:
+            items = spec.css('li')
+            for item in items:
+                key = item.css('span.spec-label::text').get()
+                if key:
+                    if item.css('ul'):
+                        values = []
+                        for value in item.css('ul li::text'):
+                            values.append(str(value).split('[] =>')[1].strip("\\n)\\n'>").strip())
+                        data[key] = ', '.join(values)
+                    elif item.css('span.te'):
+                        data[key] = ', '.join(item.css('span.te::text').getall())
+                    else:
+                        data[key] = item.css('span:nth-child(2)::text').get()
+        
+        yield {
+            'product_name': product_name,
+            'details': details,
+            'uses': uses,
+            'specifications': data,
+        }
