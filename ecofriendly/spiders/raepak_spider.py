e02c8a67210f3781c3495f2262564dcb5e36b031
rifatrakib <Rakib.1508@outlook.com>
2022-05-25 15:52:48 +0600

    crawling logic implemented for raepak spider

--- ecofriendly/spiders/raepak_spider.py
+++ ecofriendly/spiders/raepak_spider.py
@@ -7,4 +7,17 @@ class RaepakSpider(scrapy.Spider):
     start_urls = ['https://www.raepak.com/products/']
     
     def parse(self, response):
-        pass
+        for link in response.css('div.product div.col-inner a::attr(href)'):
+            yield response.follow(link, callback=self.parse_category)
+    
+    def parse_category(self, response):
+        for link in response.css('div.product div.col-inner a::attr(href)'):
+            yield response.follow(link, callback=self.parse_subcategory)
+    
+    def parse_subcategory(self, response):
+        for link in response.css('p.name a::attr(href)'):
+            if '?' not in link.get():
+                yield response.follow(link, callback=self.parse_category)
+    
+    def parse_product(self, response):
+        yield {'name': response.css('h1.product-title::text').get().strip()}
