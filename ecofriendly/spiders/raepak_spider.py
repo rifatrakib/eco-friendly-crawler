288c2094f20c6aa662f131447d19172a2fb1b75c
rifatrakib <Rakib.1508@outlook.com>
2022-05-26 19:27:25 +0600

    raepak crawler issue solved

--- ecofriendly/spiders/raepak_spider.py
+++ ecofriendly/spiders/raepak_spider.py
@@ -1,47 +1,21 @@
-import scrapy
-import pandas as pd
-from ecofriendly.items import RaepakItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-class RaepakSpider(scrapy.Spider):
+class RaepakSpider(CrawlSpider):
     name = 'raepak_spider'
     allowed_domains = ['www.raepak.com']
     start_urls = ['https://www.raepak.com/products/']
     
-    def parse(self, response):
-        for link in response.css('div.product div.col-inner a::attr(href)'):
-            yield response.follow(link, callback=self.parse_category)
+    rules = (
+        Rule(LinkExtractor(restrict_css='div.product-category div.col-inner a'), follow=True),
+        Rule(LinkExtractor(restrict_css='p.name.product-title a'), callback='parse_items'),
+    )
     
-    def parse_category(self, response):
-        for link in response.css('div.product div.col-inner a::attr(href)'):
-            yield response.follow(link, callback=self.parse_subcategory)
-    
-    def parse_subcategory(self, response):
-        for link in response.css('p.name a::attr(href)'):
-            if '?' not in link.get():
-                yield response.follow(link, callback=self.parse_product)
-    
-    def parse_product(self, response):
-        name = response.css('h1.product-title::text').get().strip()
-        details = response.css('div.product-short-description p:nth-child(1)').get()
-        category = response.css('span.posted_in a::text').get()
-        product_information = self.stringify_dict(pd.read_html(response.css('div.accordion-inner table').get())[0].to_dict(orient='records'))
-        additional_information = self.stringify_dict({row[0]: row[1] for row in pd.read_html(response.css('div.tab-panels table').get())[0].to_numpy()})
-        
-        item = RaepakItem()
-        item['name'] = name
-        item['details'] = details
-        item['category'] = category
-        item['product_information'] = product_information
-        item['additional_information'] = additional_information
-        yield item
-    
-    def stringify_dict(self, data):
-        processed_items = []
-        count = 1
-        for key, value in data.items():
-            key = key.lower().replace(' ', '_')
-            item_data = f'{count}. {key}: {value}'
-            processed_items.append(item_data)
-            count += 1
-        
-        return '\n'.join(processed_items)
+    def parse_items(self, response):
+        yield {
+            'name': response.css('h1.product-title'),
+            'category': response.css('div.product_meta span.posted_in'),
+            'description': response.css('div.product-short-description p:nth-child(1)'),
+            'dimension_sku': response.css('div.accordion-inner table'),
+            'product_information': response.css('div.tab-panels table.woocommerce-product-attributes.shop_attributes'),
+        }
